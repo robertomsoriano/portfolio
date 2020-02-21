278ffadcbce6563647e8e99c0830df63cc47fafd
Roberto <robertmsoriano@gmail.com>
2020-02-21 11:09:29 -0500

    first commit

---
+++ build/precache-manifest.e66080a7a21cfb332bac2a18bc4a3838.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2fcebace3b56d0c27a2aefc86170c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c700ec40c852b926dca2",
+    "url": "/static/css/main.e9e7ad20.chunk.css"
+  },
+  {
+    "revision": "349127784cb8cb89ce86",
+    "url": "/static/js/2.e0fb19a5.chunk.js"
+  },
+  {
+    "revision": "c700ec40c852b926dca2",
+    "url": "/static/js/main.1b8b30a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35dd79e04c8d7a23314874d4b38bdf7d",
+    "url": "/static/media/Poele.35dd79e0.png"
+  },
+  {
+    "revision": "4d52b6b992e41246dec32bac21b19bd4",
+    "url": "/static/media/ecommerce.4d52b6b9.png"
+  },
+  {
+    "revision": "42d288e8ce8db8b04a7608556fafd768",
+    "url": "/static/media/react-movies.42d288e8.jpg"
+  },
+  {
+    "revision": "8f31634adcb299cdef36ce4b8a3cc1cf",
+    "url": "/static/media/wave.8f31634a.svg"
+  },
+  {
+    "revision": "1f3453ad411e8d78cfa2ad23bcc8787f",
+    "url": "/static/media/weather.1f3453ad.jpg"
+  }
+]);
